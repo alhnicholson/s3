2b3adeca63f1f5f3abf93b5ec425089f98ff0396
alhnicholson <lien@Andreas-MacBook-Air.local>
2015-04-27 23:08:50 -0700

    html page
    html

---
+++ functions3.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+   "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+   
+<html>
+<head>
+	<title>Functions Problem 3</title>
+	<style type="text/css">
+		h1, h2, h3, h4, h5, h6 {color: #425f9e; text-align: center}
+	</style>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+</head>
+<body>
+<?php
+	function swapNumbers ($numberOne, $numberTwo)
+	{
+		$numberOne = $numberTwo;
+		$numberTwo = $numberOne;
+		return 
+	}
+	$numberOne = $_POST["numberOne"];
+	$numberTwo= $_POST["numberTwo"];
+	
+	echo "<h3>Number 1: $numberOne<br />";
+	echo "Number 2: $numberTwo<br />";
+	echo "Number 1 + Number 2 = $result</h3>" ;
+
+
+?>
+
+</body>
+</html>
